9121b9c9834db3558027466493ebc4b30487312b
Gautam Kumar <kumargautam@vmware.com>
2021-09-28 14:03:25 +0530

    recursion test

--- src/main/java/com/gtm/ds/recursion/TawersOfHanoi.java
+++ src/main/java/com/gtm/ds/recursion/TawersOfHanoi.java
@@ -7,9 +7,10 @@ public class TawersOfHanoi {
             System.out.println("Move disk-" + n + " from " + from + " to " + to);
             return;
         }
-
+        //move disk from A to B, using aux C
         move(n - 1, from, aux, to);
         System.out.println("Move disk-" + n + " from " + from + " to " + to);
+      //move disk from B to C, using aux A
         move(n - 1, aux, to, from);
 
     }
