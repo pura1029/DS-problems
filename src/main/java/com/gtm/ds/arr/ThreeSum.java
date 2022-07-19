66558ca8c8ec428edfaa2821cf19ecbfdeca4e15
kumargautam <kumargautam@vmware.com>
2022-07-19 17:43:29 +0530

    ThreeSum

---
+++ src/main/java/com/gtm/ds/arr/ThreeSum.java
@@ -0,0 +1,68 @@
+package com.gtm.ds.arr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSum {
+
+	boolean find3Numbers(int[] arr, int sum) {
+		Arrays.sort(arr);
+		int n = arr.length;
+		for (int i = 0; i < n - 2; i++) {
+			int lo = i + 1;
+			int hi = n - 1;
+
+			while (lo < hi) {
+				if (arr[i] + arr[lo] + arr[hi] == sum) {
+					System.out.println("Triplet is " + arr[i] + ", " + arr[lo] + ", " + arr[hi]);
+					return true;
+				} else if (arr[i] + arr[lo] + arr[hi] < sum) {
+					lo++;
+				} else {// arr[i]+arr[lo]+arr[hi] > sum
+					hi--;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public static List<List<Integer>> threeSum(int[] nums, int eq) {
+		Set<List<Integer>> res = new HashSet<>();
+		int n = nums.length;
+		if (n == 0) {
+			return new ArrayList<>(res);
+		}
+		Arrays.sort(nums);
+		for (int i = 0; i < n - 1; i++) {
+			int lo = i + 1;
+			int hi = n - 1;
+			while (lo < hi) {
+				int sum = nums[i] + nums[lo] + nums[hi];
+				if (sum == eq) {
+					res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+					lo++;
+					hi--;
+				} else if (sum < eq) {
+					lo++;
+				} else {// sum > eq
+					hi--;
+				}
+			}
+		}
+		return new ArrayList<>(res);
+	}
+
+	public static void main(String[] args) {
+		ThreeSum threeSum = new ThreeSum();
+		int[] arr = { 1, 4, 45, 6, 10, 8 };
+		int sum = 22;
+		threeSum.find3Numbers(arr, sum);
+
+		System.out.println(threeSum(arr, sum));
+	}
+
+}
--- src/main/java/com/gtm/ds/arr/TripletsWithZeroSum.java
+++ src/main/java/com/gtm/ds/arr/TripletsWithZeroSum.java
@@ -1,40 +1,62 @@
 package com.gtm.ds.arr;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 //https://www.geeksforgeeks.org/find-triplets-array-whose-sum-equal-zero/
 public class TripletsWithZeroSum {
 
-    // function to print triplets with 0 sum
-    static void findTriplets(int arr[], int n) {
-        boolean found = false;
+	// function to print triplets with 0 sum
+	static void findTriplets(int arr[], int n) {
+		boolean found = false;
+		for (int i = 0; i < n - 1; i++) {
+			// Find all pairs with sum equals to
+			// "-arr[i]"
+			Set<Integer> s = new HashSet<>();
+			for (int j = i + 1; j < n; j++) {
+				int x = -(arr[i] + arr[j]);// a+b+c=0 => a = -(a+b)
+				if (s.contains(x)) {
+					System.out.printf("%d %d %d\n", x, arr[i], arr[j]);
+					found = true;
+				} else {
+					s.add(arr[j]);
+				}
+			}
+		}
 
-        for (int i = 0; i < n - 1; i++) {
-            // Find all pairs with sum equals to
-            // "-arr[i]"
-            Set<Integer> s = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                int x = -(arr[i] + arr[j]);
-                if (s.contains(x)) {
-                    System.out.printf("%d %d %d\n", x, arr[i], arr[j]);
-                    found = true;
-                } else {
-                    s.add(arr[j]);
-                }
-            }
-        }
+		if (found == false) {
+			System.out.printf(" No Triplet Found\n");
+		}
+	}
 
-        if (found == false) {
-            System.out.printf(" No Triplet Found\n");
-        }
-    }
+	static boolean find3Numbers(int[] arr, int sum) {
+		Arrays.sort(arr);
+		int n = arr.length;
+		for (int i = 0; i < n - 2; i++) {
+			int lo = i + 1;
+			int hi = n - 1;
 
-    public static void main(String[] args) {
-        int arr[] = {0, -1, 2, -3, 1};
-        int n = arr.length;
-        findTriplets(arr, n);
+			while (lo < hi) {
+				if (arr[i] + arr[lo] + arr[hi] == sum) {
+					System.out.print("Triplet is " + arr[i] + ", " + arr[lo] + ", " + arr[hi]);
+					return true;
+				} else if (arr[i] + arr[lo] + arr[hi] < sum) {
+					lo++;
+				} else {// arr[i]+arr[lo]+arr[hi] > sum
+					hi--;
+				}
+			}
+		}
+		System.out.printf(" No Triplet Found\n");
+		return false;
+	}
 
-    }
+	public static void main(String[] args) {
+		int arr[] = { 0, -1, 2, -3, 1 };
+		int n = arr.length;
+		findTriplets(arr, n);
+		find3Numbers(arr, 0);
+	}
 
 }
