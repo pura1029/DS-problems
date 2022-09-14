bef44abf4ffc29dbf1c15ffd66b74a53dfadf0c2
kumargautam <kumargautam@vmware.com>
2022-09-14 17:40:36 +0530

    merge list

--- src/main/java/com/gtm/ds/inttest/MergeLists.java
+++ src/main/java/com/gtm/ds/inttest/MergeLists.java
@@ -22,30 +22,32 @@ public class MergeLists {
 
 		@Override
 		public int hashCode() {
-			int hash = 1;
-			hash = hash * 31 + this.id;
-			hash = hash * 31 + this.name.hashCode();
-			return hash;
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + id;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
 		}
 
 		@Override
-		public boolean equals(Object oth) {
-			if (this == oth)
+		public boolean equals(Object obj) {
+			if (this == obj)
 				return true;
-			if (oth == null) {
+			if (obj == null)
 				return false;
-			}
-			if (getClass() != oth.getClass())
+			if (getClass() != obj.getClass())
 				return false;
-			Product other = (Product) oth;
-			if (this.id != other.id) {
+			Product other = (Product) obj;
+			if (id != other.id)
 				return false;
-			}
-			if (!this.name.equals(other.name)) {
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
 				return false;
-			}
 			return true;
 		}
+
 	}
 
 	public static void main(String[] args) {
