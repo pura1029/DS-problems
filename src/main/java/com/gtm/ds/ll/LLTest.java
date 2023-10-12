5a5d6d0acc7770bc0a35aabacf3ffa59fd745e49
Gautam Kumar <kumargautam@vmware.com>
2023-10-12 16:13:51 +0530

    add two Linked list

--- src/main/java/com/gtm/ds/ll/LLTest.java
+++ src/main/java/com/gtm/ds/ll/LLTest.java
@@ -2,305 +2,305 @@ package com.gtm.ds.ll;
 
 public class LLTest {
 
-	Node head;
+    Node head;
 
-	private static class Node {
-		int data;
-		Node next;
+    private static class Node {
+        int data;
+        Node next;
 
-		Node(int data) {
-			this.data = data;
-			this.next = null;
-		}
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
 
-		@Override
-		public String toString() {
-			return "Node [data=" + data + "]";
-		}
-	}
+        @Override
+        public String toString() {
+            return "Node [data=" + data + "]";
+        }
+    }
 
-	public void addFirst(int data) {
-		Node newNode = new Node(data);
-		if (head == null) {
-			head = newNode;
-			return;
-		}
-		newNode.next = head;
-		head = newNode;
-	}
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
 
-	public void addLast(Node newNode) {
-		if (head == null) {
-			head = newNode;
-			return;
-		}
-		Node curr = head;
-		while (curr.next != null) {
-			curr = curr.next;
-		}
-		curr.next = newNode;
-	}
+    public void addLast(Node newNode) {
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+    }
 
-	public void addLast(int data) {
-		Node newNode = new Node(data);
-		addLast(newNode);
-	}
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        addLast(newNode);
+    }
 
-	public void print() {
-		Node curr = head;
-		while (curr != null) {
-			System.out.print(curr.data);
-			if (curr.next != null) {
-				System.out.print("->");
-			}
-			curr = curr.next;
-		}
+    public void print() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data);
+            if (curr.next != null) {
+                System.out.print("->");
+            }
+            curr = curr.next;
+        }
 
-		System.out.println();
-	}
+        System.out.println();
+    }
 
-	public void deleteFirst() {
-		if (head == null) {
-			System.out.println("List empty");
-			return;
-		}
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("List empty");
+            return;
+        }
 
-		head = head.next;
-	}
+        head = head.next;
+    }
 
-	public void deleteLast() {
-		if (head == null) {
-			System.out.println("List empty");
-			return;
-		}
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("List empty");
+            return;
+        }
 
-		if (head.next == null) {
-			head = null;
-			return;
-		}
+        if (head.next == null) {
+            head = null;
+            return;
+        }
 
-		Node prev = head;
-		Node curr = head.next;
+        Node prev = head;
+        Node curr = head.next;
 
-		while (curr.next != null) {
-			prev = curr;
-			curr = curr.next;
-		}
+        while (curr.next != null) {
+            prev = curr;
+            curr = curr.next;
+        }
 
-		prev.next = null;
-	}
+        prev.next = null;
+    }
 
-	public void reverse() {
-		if (head == null || head.next == null) {
-			return;
-		}
-		Node prev = head;
-		Node curr = head.next;
-		while (curr != null) {
-			Node next = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = next;
-		}
+    public void reverse() {
+        if (head == null || head.next == null) {
+            return;
+        }
+        Node prev = head;
+        Node curr = head.next;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
 
-		head.next = null;
-		head = prev;
-	}
+        head.next = null;
+        head = prev;
+    }
 
-	public Node reverseStack(Node head) {
-		if (head == null || head.next == null) {
-			return head;
-		}
-		Node newHead = reverseStack(head.next);
-		head.next.next = head;
-		head.next = null;
+    public Node reverseStack(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node newHead = reverseStack(head.next);
+        head.next.next = head;
+        head.next = null;
 
-		return newHead;
-	}
+        return newHead;
+    }
 
-	public Node removeNthFromEnd(Node head, int n) {
-		if (head.next == null) {
-			return null;
-		}
+    public Node removeNthFromEnd(Node head, int n) {
+        if (head.next == null) {
+            return null;
+        }
 
-		int size = 0;
-		Node curr = head;
-		while (curr != null) {
-			size++;
-			curr = curr.next;
-		}
-		if (size < n) {
-			return head;
-		}
+        int size = 0;
+        Node curr = head;
+        while (curr != null) {
+            size++;
+            curr = curr.next;
+        }
+        if (size < n) {
+            return head;
+        }
 
-		if (size == n) {
-			return head.next;
-		}
+        if (size == n) {
+            return head.next;
+        }
 
-		int prevNthNode = size - n;
+        int prevNthNode = size - n;
 
-		Node prev = head;
-		int i = 1;
-		while (i < prevNthNode) {
-			prev = prev.next;
-			i++;
-		}
-		prev.next = prev.next.next;
-		return head;
-	}
+        Node prev = head;
+        int i = 1;
+        while (i < prevNthNode) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return head;
+    }
 
-	public boolean isPlindrome(Node head) {
-		if (head == null || head.next == null) {
-			return true;
-		}
+    public boolean isPlindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
 
-		Node middle = findMiddle(head);
-		Node secondHalfStart = reverse(middle.next);
+        Node middle = findMiddle(head);
+        Node secondHalfStart = reverse(middle.next);
 
-		Node firstHalfStart = head;
-		while (secondHalfStart != null) {
-			if (secondHalfStart.data != firstHalfStart.data) {
-				return false;
-			}
-			firstHalfStart = firstHalfStart.next;
-			secondHalfStart = secondHalfStart.next;
-		}
-		return true;
-	}
+        Node firstHalfStart = head;
+        while (secondHalfStart != null) {
+            if (secondHalfStart.data != firstHalfStart.data) {
+                return false;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+        return true;
+    }
 
-	private Node reverse(Node middle) {
-		Node prev = null;
-		Node curr = middle;
-		while (curr != null) {
-			Node next = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = next;
-		}
+    private Node reverse(Node middle) {
+        Node prev = null;
+        Node curr = middle;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
 
-		return prev;
-	}
+        return prev;
+    }
 
-	private Node findMiddle(Node head1) {
-		Node slow = head1;
-		Node fast = head1;
+    private Node findMiddle(Node head1) {
+        Node slow = head1;
+        Node fast = head1;
 
-		while (fast.next != null && fast.next.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
 
-		return slow;
-	}
+        return slow;
+    }
 
-	public boolean hasCycle(Node curr) {
-		if (head == null) {
-			return false;
-		}
-		Node slow = curr;
-		Node fast = curr;
-		while (fast != null && fast.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
+    public boolean hasCycle(Node curr) {
+        if (head == null) {
+            return false;
+        }
+        Node slow = curr;
+        Node fast = curr;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
 
-			if (slow == fast) {
-				return true;
-			}
-		}
+            if (slow == fast) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public Node findCycleStartNode(Node curr) {
-		if (head == null) {
-			return null;
-		}
-		Node slow = curr;
-		Node fast = curr;
-		while (fast != null && fast.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-			if (slow == fast) {
-				break;
-			}
-		}
+    public Node findCycleStartNode(Node curr) {
+        if (head == null) {
+            return null;
+        }
+        Node slow = curr;
+        Node fast = curr;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
 
-		slow = head;
+        slow = head;
 
-		while (fast != null) {
-			slow = slow.next;
-			fast = fast.next;
-			if (slow == fast) {
-				return slow;
-			}
-		}
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public Node removeCycle(Node curr) {
-		if (head == null) {
-			return null;
-		}
-		Node slow = curr;
-		Node fast = curr;
-		boolean foundCycle = false;
-		while (fast != null && fast.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-			if (slow == fast) {
-				foundCycle = true;
-				break;
-			}
-		}
+    public Node removeCycle(Node curr) {
+        if (head == null) {
+            return null;
+        }
+        Node slow = curr;
+        Node fast = curr;
+        boolean foundCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                foundCycle = true;
+                break;
+            }
+        }
 
-		slow = head;
-		Node prev = null;
-		while (foundCycle) {
-			slow = slow.next;
-			prev = fast;
-			fast = fast.next;
-			if (slow == fast) {
-				break;
-			}
-		}
+        slow = head;
+        Node prev = null;
+        while (foundCycle) {
+            slow = slow.next;
+            prev = fast;
+            fast = fast.next;
+            if (slow == fast) {
+                break;
+            }
+        }
 
-		if (foundCycle && fast != null) {
-			prev.next = null;
-		}
+        if (foundCycle && fast != null) {
+            prev.next = null;
+        }
 
-		return curr;
-	}
+        return curr;
+    }
 
-	public static void main(String[] args) {
-		LLTest test = new LLTest();
-		test.addFirst(2);
-		test.addFirst(1);
-		Node node3 = new Node(3);
-		test.addLast(node3);
-		test.addLast(4);
-		test.addLast(5);
-		test.addLast(node3);
-		//test.print();//1->2->3->4->5
+    public static void main(String[] args) {
+        LLTest test = new LLTest();
+        test.addFirst(2);
+        test.addFirst(1);
+        Node node3 = new Node(3);
+        test.addLast(node3);
+        test.addLast(4);
+        test.addLast(5);
+        test.addLast(node3);
+        //test.print();//1->2->3->4->5
 
-		// test.deleteFirst();
-		// test.print();
-		// test.deleteLast();
-		// test.print();
-		// test.head = test.reverseStack(test.head);
-		// test.print();
+        // test.deleteFirst();
+        // test.print();
+        // test.deleteLast();
+        // test.print();
+        // test.head = test.reverseStack(test.head);
+        // test.print();
 
-		// test.head = test.removeNthFromEnd(test.head, 2);
-		// test.print();
+        // test.head = test.removeNthFromEnd(test.head, 2);
+        // test.print();
 
-		// System.out.println("isPlindrome :"+test.isPlindrome(test.head));
+        // System.out.println("isPlindrome :"+test.isPlindrome(test.head));
 
-		System.out.println("hasCycle :" + test.hasCycle(test.head));
+        System.out.println("hasCycle :" + test.hasCycle(test.head));
 
-		// System.out.println("Cycle Node :" + test.findCycleStartNode(test.head));
+        // System.out.println("Cycle Node :" + test.findCycleStartNode(test.head));
 
-		 test.removeCycle(test.head);
-		 test.print();
-	}
+        test.removeCycle(test.head);
+        test.print();
+    }
 
 }
--- src/main/java/com/gtm/ds/ll/Sum2LL.java
+++ src/main/java/com/gtm/ds/ll/Sum2LL.java
@@ -3,63 +3,66 @@ package com.gtm.ds.ll;
 //https://leetcode.com/problems/add-two-numbers/
 public class Sum2LL {
 
-	ListNode ansHead = null;
-	ListNode ansTail = null;
+    ListNode ansHead = null;
+    ListNode ansTail = null;
 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-		int carry = 0;
-		while (l1 != null || l2 != null) {
-			int sum = 0;
-			if (l1 != null) {
-				sum += l1.data;
-				l1 = l1.next;
-			}
-			if (l2 != null) {
-				sum += l2.data;
-				l2 = l2.next;
-			}
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.data;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.data;
+                l2 = l2.next;
+            }
 
-			sum += carry;
+            sum += carry;
 
-			carry = sum / 10;
-			int newVal = sum % 10;
-			addNewNode(newVal);
-		}
+            carry = sum / 10;
+            int newVal = sum % 10;
+            addNewNode(newVal);
+        }
 
-		if (carry != 0) {
-			addNewNode(carry);
-		}
+        if (carry != 0) {
+            addNewNode(carry);
+        }
 
-		return ansHead;
-	}
+        return ansHead;
+    }
 
-	private void addNewNode(int newVal) {
-		ListNode newNode = new ListNode(newVal);
-		if (ansTail == null) {
-			ansHead = newNode;
-			ansTail = newNode;
-			return;
-		}
-		ansTail.next = newNode;
-		ansTail = newNode;
-	}
+    // Add node to last
+    private void addNewNode(int newVal) {
+        ListNode newNode = new ListNode(newVal);
+        if (ansTail == null) {
+            ansHead = newNode;
+            ansTail = newNode;
+            return;
+        }
+        ansTail.next = newNode;
+        ansTail = newNode;
+    }
 
-	public static void main(String[] args) {
-		LinkedListTest linkedListTest1 = new LinkedListTest();
-		linkedListTest1.insert(3);
-		linkedListTest1.insert(4);
-		linkedListTest1.insert(2);
+    public static void main(String[] args) {
+        LinkedListTest linkedListTest1 = new LinkedListTest();
+        linkedListTest1.insert(3);
+        linkedListTest1.insert(4);
+        linkedListTest1.insert(2);
 
-		LinkedListTest linkedListTest2 = new LinkedListTest();
-		linkedListTest2.insert(4);
-		linkedListTest2.insert(6);
-		linkedListTest2.insert(5);
+        LinkedListTest linkedListTest2 = new LinkedListTest();
+        linkedListTest2.insert(7);
+        linkedListTest2.insert(6);
+        linkedListTest2.insert(5);
+        linkedListTest1.print();
+        linkedListTest2.print();
 
-		Sum2LL sum2ll = new Sum2LL();
+        Sum2LL sum2ll = new Sum2LL();
 
-		ListNode listNode = sum2ll.addTwoNumbers(linkedListTest1.getHead(), linkedListTest2.getHead());
-		linkedListTest1.print(listNode);
-	}
+        ListNode listNode = sum2ll.addTwoNumbers(linkedListTest1.getHead(), linkedListTest2.getHead());
+        linkedListTest1.print(listNode);
+    }
 
 }
