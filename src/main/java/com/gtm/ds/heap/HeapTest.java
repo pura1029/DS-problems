40f9fd28216d171811b6bf179c1cb06c15c36d86
Gautam Kumar <kumargautam@vmware.com>
2023-10-12 18:47:29 +0530

    max heap

--- src/main/java/com/gtm/ds/heap/HeapTest.java
+++ src/main/java/com/gtm/ds/heap/HeapTest.java
@@ -2,128 +2,180 @@ package com.gtm.ds.heap;
 
 public class HeapTest {
 
-	static int insertNode(int[] arr, int n, int key) {
-		n = n + 1;
-		arr[n - 1] = key;
+    static int insertNode(int[] arr, int n, int key) {
+        n = n + 1;
+        arr[n - 1] = key;
 
-		// Heapify the new node following a
-		// Bottom-up approach
-		heapify(arr, n, n - 1);
+        // Heapify the new node following a
+        // Bottom-up approach
+        heapify(arr, n - 1);
 
-		return n;
-	}
+        return n;
+    }
 
-	/**
-	 * Heapify the new node following a Bottom-up approach
-	 * 
-	 * @param arr
-	 * @param n
-	 * @param i
-	 */
-	private static void heapify(int[] arr, int n, int i) {
-		// Find parent
-		int parent = (i - 1) / 2;
+    static int insertNode1(int[] arr, int n, int key) {
+        n = n + 1;
+        int i = n - 1;
+        arr[i] = key;
 
-		// For Max-Heap
-		// If current node is greater than its parent
-		// Swap both of them and call heapify again
-		// for the parent
-		if (parent >= 0 && arr[i] > arr[parent]) {
-			swap(arr, i, parent);
+        // Heapify the new node following a
+        // Bottom-up approach
+        // For Max-Heap
+        // If current node is greater than its parent
+        // Swap both of them and call heapify again
+        // for the parent
+        while (i > 0) {
+            int parent = (i - 1) / 2;
+            if (arr[parent] < arr[i]) {
+                swap(arr, i, parent);
+                i = parent;
+            } else {
+                return n;
+            }
+        }
 
-			// Recursively heapify the parent node
-			heapify(arr, n, parent);
-		}
+        return n;
+    }
 
-	}
+    /**
+     * Heapify the new node following a Bottom-up approach
+     *
+     * @param arr
+     * @param i
+     */
+    private static void heapify(int[] arr, int i) {
+        // Find parent
+        int parent = (i - 1) / 2;
 
-	private static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+        // For Max-Heap
+        // If current node is greater than its parent
+        // Swap both of them and call heapify again
+        // for the parent
+        if (parent >= 0 && arr[i] > arr[parent]) {
+            swap(arr, i, parent);
 
-	static void printArray(int[] arr, int n) {
-		for (int i = 0; i < n; ++i)
-			System.out.println(arr[i] + " ");
+            // Recursively heapify the parent node
+            heapify(arr, parent);
+        }
 
-		System.out.println();
-	}
+    }
 
-	static int deleteRoot(int arr[], int n) {
-		int lastElement = arr[n - 1];
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
-		// Replace root with first element
-		arr[0] = lastElement;
+    static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; ++i)
+            System.out.println(arr[i] + " ");
 
-		// Decrease size of heap by 1
-		n = n - 1;
+        System.out.println();
+    }
 
-		// heapify the root node
-		heapifyForDelete(arr, n, 0);
+    static int deleteRoot(int arr[], int n) {
+        int lastElement = arr[n - 1];
 
-		// return new size of Heap
-		return n;
-	}
+        // Replace root with first element
+        arr[0] = lastElement;
 
-	// To heapify a subtree rooted with node i which is
-	// an index in arr[].and n is size of heap
-	static void heapifyForDelete(int arr[], int n, int i) {
-		int largest = i; // Initialize largest as root
-		int l = 2 * i + 1; // left = 2*i + 1
-		int r = 2 * i + 2; // right = 2*i + 2
+        // Decrease size of heap by 1
+        n = n - 1;
 
-		// If left child is larger than root
-		if (l < n && arr[l] > arr[largest]) {
-			largest = l;
-		}
+        // heapify the root node
+        heapifyForDelete(arr, n, 0);
 
-		// If right child is larger than largest so far
-		if (r < n && arr[r] > arr[largest]) {
-			largest = r;
-		}
+        // return new size of Heap
+        return n;
+    }
 
-		// If largest is not root
-		if (largest != i) {
-			swap(arr, i, largest);
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[].and n is size of heap
+    static void heapifyForDelete(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
 
-			// Recursively heapify the affected sub-tree
-			heapifyForDelete(arr, n, largest);
-		}
-	}
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
 
-	public static void main(String[] args) {
-		int MAX = 1000;
-		int[] arr = new int[MAX];
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
 
-		// initializing some values
-		arr[0] = 10;
-		arr[1] = 5;
-		arr[2] = 3;
-		arr[3] = 2;
-		arr[4] = 4;
-		int key = 15;
-		int n = 5;
+        // If largest is not root
+        if (largest != i) {
+            swap(arr, i, largest);
 
-		n = insertNode(arr, n, key);
-		printArray(arr, n);
-		// Final Heap will be:
-		// 15
-		// / \
-		// 5 10
-		// / \ /
-		// 2 4 3
+            // Recursively heapify the affected sub-tree
+            heapifyForDelete(arr, n, largest);
+        }
+    }
 
-		n = deleteRoot(arr, n);
+    static int deleteNode(int arr[], int n) {
+        int lastElement = arr[n - 1];
 
-		printArray(arr, n);
+        // Replace root with first element
+        arr[0] = lastElement;
 
-		// Final Heap will be:
-		// 10
-		// / \
-		// 5 3
-		// / \
-		// 2 4
-	}
+        // Top-Bottom approach
+        // Decrease size of heap by 1
+        // For Max-Heap
+        // If current node is less than its parent
+        // Swap both of them and call heapify again
+        // for the larger
+        n = n - 1;
+        int i = 0;
+        while (i < n) {
+            int l = 2 * i + 1; // left = 2*i + 1
+            int r = 2 * i + 2; // right = 2*i + 2
+            int larger = arr[l] > arr[r] ? l : r;
+            if (arr[i] < arr[larger]) {
+                swap(arr, i, larger);
+                i = larger;
+            } else {
+                return n;
+            }
+        }
+
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int MAX = 1000;
+        int[] arr = new int[MAX];
+
+        // initializing some values
+        arr[0] = 10;
+        arr[1] = 5;
+        arr[2] = 3;
+        arr[3] = 2;
+        arr[4] = 4;
+        int key = 15;
+        int n = 5;
+
+        n = insertNode(arr, n, key);
+        printArray(arr, n);
+        // Final Heap will be:
+        // 15
+        // / \
+        // 5 10
+        // / \ /
+        // 2 4 3
+
+        n = deleteRoot(arr, n);
+
+        printArray(arr, n);
+
+        // Final Heap will be:
+        // 10
+        // / \
+        // 5 3
+        // / \
+        // 2 4
+    }
 
 }
