c7368a0d0334d2a6dc31237aec57f1abc5e54ef6
Gautam Kumar <kumargautam@vmware.com>
2023-10-13 12:23:01 +0530

    adapter design pattern

---
+++ src/main/java/com/gtm/ds/design/adapter/IMicroUsbPhone.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 VMware, Inc. All Rights Reserved.
+ *
+ */
+
+package com.gtm.ds.design.adapter;
+
+public interface IMicroUsbPhone {
+    void recharge();
+
+    void useMicroUsb();
+}
---
+++ src/main/java/com/gtm/ds/design/adapter/Iphone.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 VMware, Inc. All Rights Reserved.
+ *
+ */
+
+package com.gtm.ds.design.adapter;
+
+public class Iphone implements ILightningPhone {
+    private boolean connector;
+
+    @Override
+    public void useLightning() {
+        connector = true;
+        System.out.println("Lightning connected");
+    }
+
+    @Override
+    public void recharge() {
+        if (connector) {
+            System.out.println("Recharge started");
+            System.out.println("Recharge finished");
+        } else {
+            System.out.println("Connect Lightning first");
+        }
+    }
+}
