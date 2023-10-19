a0006ee999416406b9b242dbfa2fa5bcf6041cb1
Gautam Kumar <kumargautam@vmware.com>
2023-10-19 23:20:35 +0530

    Graph Data Structure.pdf

--- src/main/java/com/gtm/ds/graph2/GraphTest.java
+++ src/main/java/com/gtm/ds/graph2/GraphTest.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+//https://www.youtube.com/watch?v=59fUtYYz7ZU&list=PLfqMhTWNBTe0sPLFF91REaJQEteFZtLzA&index=15&ab_channel=ApnaCollege
 public class GraphTest {
 
     private List<List<Edge>> graph;
@@ -16,6 +17,10 @@ public class GraphTest {
     public GraphTest(int v) {
         this.graph = new ArrayList<>(v);
         this.V = v;
+        for (int i = 0; i < V; i++) {
+            List<Edge> edges = new ArrayList<>();
+            graph.add(edges);
+        }
     }
 
     @AllArgsConstructor
@@ -32,11 +37,6 @@ public class GraphTest {
     }
 
     public void createGraph() {
-
-        for (int i = 0; i < V; i++) {
-            List<Edge> edges = new ArrayList<>();
-            graph.add(edges);
-        }
 
         addEdge(0, 0, 1, 1);
         addEdge(0, 0, 2, 2);
@@ -60,6 +60,16 @@ public class GraphTest {
         addEdge(5, 5, 6, 4);
 
         addEdge(6, 6, 5, 5);
+    }
+
+    public void createDirectedGraph() {
+        addEdge(0, 0, 2, 1);
+
+        addEdge(1, 1, 0, 2);
+
+        addEdge(2, 2, 3, -1);
+
+        addEdge(3, 3, 0, 4);
     }
 
     public void bfs(boolean[] visited, int start) {
@@ -93,6 +103,24 @@ public class GraphTest {
         }
     }
 
+    public boolean detectCycleinDirectedGraph(int curr, boolean[] visited, boolean[] stack) {
+
+        stack[curr] = true;
+        visited[curr] = true;
+
+        for (int i = 0; i < graph.get(curr).size(); i++) {
+            Edge edge = graph.get(curr).get(i);
+            if (stack[edge.getDest()]) {
+                return true;
+            }
+            if (!visited[edge.getDest()] && detectCycleinDirectedGraph(edge.dest, visited, stack)) {
+                return true;
+            }
+        }
+        stack[curr] = false;
+        return false;
+    }
+
     public void printAllPath(int curr, int target, String path, boolean[] visited) {
 
         if (curr == target) {
@@ -109,6 +137,101 @@ public class GraphTest {
             }
         }
 
+    }
+
+    //https://leetcode.com/problems/number-of-islands/
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int visited[][] = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                visited[i][j] = 0;
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (visited[i][j] == 0 && grid[i][j] == '1') {
+                    count++;
+                    dfsForNumIslands(grid, i, j, visited);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfsForNumIslands(char grid[][], int row, int col, int visited[][]) {
+        visited[row][col] = 1;
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        /**
+         *          (r-1,c)
+         *  (r,c-1) (r,c) (r,c+1)
+         *         (r+1,c)
+         */
+
+        int delrow[] = {-1, 0, 1, 0};// Array to represent possible row moves (up, right, down, left)
+        int delcol[] = {0, 1, 0, -1};// Array to represent possible column moves (up, right, down, left)
+        for (int i = 0; i < 4; i++) {
+
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
+                    && visited[nrow][ncol] == 0 && grid[nrow][ncol] == '1') {
+                // Recursively call DFS on the adjacent cell
+                dfsForNumIslands(grid, nrow, ncol, visited);
+            }
+        }
+    }
+
+    //https://leetcode.com/problems/rotting-oranges/
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<Pair> queue = new LinkedList<Pair>();
+        int[][] vis = new int[n][m];
+        int countFresh = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new Pair(i, j, 0));
+                    vis[i][j] = 2;
+                } else {
+                    vis[i][j] = 0;
+                }
+                if (grid[i][j] == 1) countFresh++;
+            }
+        }
+        int tm = 0;
+        int[] drow = {-1, 0, 1, 0};// Array to represent possible row moves (up, right, down, left)
+        int[] dcol = {0, 1, 0, -1};// Array to represent possible column moves (up, right, down, left)
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            int r = queue.peek().row;
+            int c = queue.peek().col;
+            int t = queue.peek().tm;
+            tm = Math.max(tm, t);
+            queue.remove();
+            for (int i = 0; i < 4; i++) {
+                int nRow = r + drow[i];
+                int nCol = c + dcol[i];
+                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m
+                        && vis[nRow][nCol] == 0 && grid[nRow][nCol] == 1) {
+                    queue.add(new Pair(nRow, nCol, t + 1));
+                    vis[nRow][nCol] = 2;
+                    cnt++;
+                }
+            }
+        }
+        if (cnt != countFresh) return -1;
+        return tm;
     }
 
     public static void main(String[] args) {
@@ -130,6 +253,40 @@ public class GraphTest {
 
         visited = new boolean[V];
         graphTest.printAllPath(src, dest, ("" + src), visited);
+
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        System.out.println("num of Islands :" + graphTest.numIslands(grid));
+
+        int[][] grid1 = {
+                {2, 1, 1},
+                {1, 1, 0},
+                {0, 1, 1}
+        };
+        System.out.println("time for orangesRotting :" + graphTest.orangesRotting(grid1));
+
+        //Directed graph
+        int v1 = 4;
+        GraphTest directedGraphTest = new GraphTest(v1);
+        directedGraphTest.createDirectedGraph();
+
+        System.out.println("is cycle:" + directedGraphTest.detectCycleinDirectedGraph(0, new boolean[v1], new boolean[v1]));
     }
 
+    class Pair {
+        int row;
+        int col;
+        int tm;
+
+        Pair(int row, int col, int tm) {
+            this.row = row;
+            this.col = col;
+            this.tm = tm;
+        }
+    }
 }
