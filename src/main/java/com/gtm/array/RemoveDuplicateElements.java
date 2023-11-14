e58852c4ec895340197cc459381d512b1d3ddbd9
Gautam Kumar <kumargautam@vmware.com>
2023-11-14 17:49:14 +0530

    decimal

--- src/main/java/com/gtm/array/RemoveDuplicateElements.java
+++ src/main/java/com/gtm/array/RemoveDuplicateElements.java
@@ -5,104 +5,101 @@ import java.util.Arrays;
 public class RemoveDuplicateElements {
 
 
-	// Function to remove duplicate elements
-	// This function returns new size of modified
-	// array.
-	static int removeDuplicates(int arr[], int n)
-	{
-		if(n == 0 || n == 1)
-			return n;
-		// To store index of next unique element
-		int j = 0;
-		// If current element is not equal
-		// to next element then store that
-		// current element
-		for (int i = 0; i < n-1; i++) 
-			if(arr[i] != arr[i+1])
-				arr[j++] = arr[i];
+    // Function to remove duplicate elements
+    // This function returns new size of modified
+    // array.
+    static int removeDuplicates(int arr[], int n) {
+        if (n == 0 || n == 1)
+            return n;
+        // To store index of next unique element
+        int j = 0;
+        // If current element is not equal
+        // to next element then store that
+        // current element
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] != arr[i + 1])
+                arr[j++] = arr[i];
 
-		// Store the last element as whether
-		// it is unique or repeated, it hasn't
-		// stored previously
-		arr[j++] = arr[n-1];
-		return j;
-	}
+        // Store the last element as whether
+        // it is unique or repeated, it hasn't
+        // stored previously
+        arr[j++] = arr[n - 1];
+        return j;
+    }
 
-	static String removeDuplicatesCar(String s){
-		char ch[] = s.toCharArray();
-		int n = ch.length;
-		if(n == 0 || n == 1)
-			return s;
-		int j = 0;
-		for (int i = 0; i < n-1; i++) 
-			if(ch[i] != ch[i+1])
-				ch[j++] = ch[i];
+    static String removeDuplicatesCar(String s) {
+        char ch[] = s.toCharArray();
+        int n = ch.length;
+        if (n == 0 || n == 1)
+            return s;
+        int j = 0;
+        for (int i = 0; i < n - 1; i++)
+            if (ch[i] != ch[i + 1])
+                ch[j++] = ch[i];
 
-		ch[j++] = ch[n-1];
-		ch = Arrays.copyOf(ch, j);	
-		return String.valueOf(ch);
-	}
+        ch[j++] = ch[n - 1];
+        ch = Arrays.copyOf(ch, j);
+        return String.valueOf(ch);
+    }
 
-	static int removeDuplicates1(int arr[], int n)
-	{
-		if(n == 0 || n == 1)
-			return n;
-		int i = 1, j = 0;
+    static int removeDuplicates1(int arr[], int n) {
+        if (n == 0 || n == 1)
+            return n;
+        int i = 1, j = 0;
 
-		while (i  < n) {
-			if (arr[j] == arr[i]) {
-				i++;
-			} else {
-				arr[++j] = arr[i++];
-			}
-		}
-		return j+1;
-	}
+        while (i < n) {
+            if (arr[j] == arr[i]) {
+                i++;
+            } else {
+                arr[++j] = arr[i++];
+            }
+        }
+        return j + 1;
+    }
 
-	static int removeDuplicates2(int arr[], int n)
-	{
-		if(n == 0 || n == 1)
-			return n;
+    static int removeDuplicates2(int arr[], int n) {
+        if (n == 0 || n == 1)
+            return n;
 
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1 ; j < n; j++) {
-				if (arr[i] == (arr[j])) {
-					// got the duplicate element
-					arr[j] = arr[n-1];
-					n--;
-					j--;
-				}
-			}
-		}
-		return n;
-	}
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == (arr[j])) {
+                    // got the duplicate element
+                    arr[j] = arr[n - 1];
+                    n--;
+                    j--;
+                }
+            }
+        }
+        return n;
+    }
 
-	public static void main(String[] args) {
-		int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
-		int arr1[] = {3,3,1,2,2,1, 4, 5, 5,4};
-		int n = arr.length;
-		int n1 = arr1.length;
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+        int arr1[] = {3, 3, 1, 2, 2, 1, 4, 5, 5, 4};
+        int n = arr.length;
+        int n1 = arr1.length;
 
-		n = removeDuplicates(arr, n);
+        n = removeDuplicates(arr, n);
 
-		// Print updated array
-		for (int i=0; i<n; i++)
-			System.out.print(arr[i]+" ");
+        // Print updated array
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
 
-		System.out.println("\n"+removeDuplicatesCar("abccdddee"));
+        System.out.println("\n" + removeDuplicatesCar("abccdddee"));
 
-		int n2 = removeDuplicates1(arr, n);
-		System.out.println("using removeDuplicates1 :");
-		// Print updated array
-		for (int i=0; i<n2; i++)
-			System.out.print(arr[i]+" ");
+        int n2 = removeDuplicates1(arr, n);
+        System.out.println("using removeDuplicates1 :");
+        // Print updated array
+        for (int i = 0; i < n2; i++)
+            System.out.print(arr[i] + " ");
 
-		int n3 = removeDuplicates2(arr1, n1);
-		System.out.println("\nusing removeDuplicates2 :");
-		// Print updated array
-		for (int i=0; i<n3; i++)
-			System.out.print(arr1[i]+" ");
+        int n3 = removeDuplicates2(arr1, n1);
+        System.out.println("\nusing removeDuplicates2 :");
+        // Print updated array
+        for (int i = 0; i < n3; i++)
+            System.out.print(arr1[i] + " ");
 
-	}
+    }
 
 }
---
+++ src/main/java/com/gtm/ds/inttest/BinaryDecimalConverter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 VMware, Inc. All Rights Reserved.
+ *
+ */
+
+package com.gtm.ds.inttest;
+
+/**
+ * Insert your comment for {@link BinaryDecimalConverter}.
+ *
+ * @author kumargautam
+ */
+public class BinaryDecimalConverter {
+
+    public long convertDecimalToBinary(long n) {
+
+        long binaryNumber = 0;
+        int remainder, i = 1, step = 1;
+        while (n > 0) {
+            remainder = (int) (n % 2);
+            System.out.println("Step " + step++ + ": " + n + "/2");
+
+            System.out.println("Quotient = " + n / 2 + ", Remainder = " + remainder);
+            n /= 2;
+
+            binaryNumber += remainder * i;
+            i *= 10;
+        }
+
+        return binaryNumber;
+    }
+
+
+    public long convertBinaryToDecimal(long binary) {
+        long sum = 0;
+        int power = 1;
+        while (binary > 0) {
+            long temp = binary % 10;
+            sum = sum + (temp * power);
+            power *= 2;
+            binary = binary / 10;
+        }
+        return sum;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        BinaryDecimalConverter converter = new BinaryDecimalConverter();
+        long decimal = 8;
+        long binary = converter.convertDecimalToBinary(decimal);
+        System.out.println("Decimal value of " + decimal + ", binary is :" + binary);
+        System.out.println("Binary No of " + binary + ", decimal is :" + converter.convertBinaryToDecimal(binary));
+    }
+}
