6d35965469216911d0515d99e756f37ddeadeb99
Gautam Kumar <kumargautam@vmware.com>
2024-05-03 11:37:25 +0530

    longestUniqueSubStr

--- src/main/java/com/gtm/ds/str/v1/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/gtm/ds/str/v1/LongestSubstringWithoutRepeatingCharacters.java
@@ -12,6 +12,7 @@ import java.util.Set;
 
 /**
  * https://takeuforward.org/data-structure/length-of-longest-substring-without-any-repeating-character/
+ * https://www.youtube.com/watch?v=Uf6YUcbctIk
  *
  * @author kumargautam
  */
@@ -22,6 +23,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int maxans = Integer.MIN_VALUE;
         Set<Character> set = new HashSet<>();
         int l = 0;
+        String ans = "";
         for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
         {
             if (set.contains(str.charAt(r))) //if duplicate element is found
@@ -33,7 +35,9 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
             set.add(str.charAt(r));
             maxans = Math.max(maxans, r - l + 1);
+            ans = str.substring(l, r + 1);
         }
+        System.out.println("longestUniqueSubsttr :" + ans);
         return maxans;
     }
 
@@ -45,6 +49,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int r = 0;
         Map<Character, Integer> map = new HashMap<>();// <character,index>
         int counter = 0;
+        String ans = "";
         while (l < str.length() && r < str.length()) {
             char ch = str.charAt(r);
             if (map.containsKey(ch)) { //if duplicate element is found, update left pointer
@@ -52,8 +57,38 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
             map.put(ch, r);// update current char index
             counter = Math.max(counter, r - l + 1);
+            ans = str.substring(l, r + 1);
             r++;
         }
+        System.out.println("longestUniqueSubStr :" + ans);
+        return counter;
+    }
+
+    public static int longest2UniqueSubStr(String str) {// using sliding window
+        if (str.length() == 0)
+            return 0;
+
+        int l = 0;
+        int r = 0;
+        Map<Character, Integer> map = new HashMap<>();// <character,count>
+        int counter = 0;
+        String ans = "";
+        while (l < str.length() && r < str.length()) {
+            char ch = str.charAt(r);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            while (map.size() > 2) {
+                char lCh = str.charAt(l++);
+                map.put(lCh, map.get(lCh) - 1);
+                if (map.get(lCh) == 0) {
+                    map.remove(lCh);
+                }
+            }
+
+            counter = Math.max(counter, r - l + 1);
+            ans = str.substring(l, r + 1);
+            r++;
+        }
+        System.out.println("longest2UniqueSubStr :" + ans);
         return counter;
     }
 
@@ -62,5 +97,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
         System.out.println(longestUniqueSubsttr(str));
         System.out.println(longestUniqueSubStr(str));
+        String string = "ccaabbb";
+        System.out.println(longest2UniqueSubStr(string));
     }
 }
